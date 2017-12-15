fe0b39a3ef19bdd61caac11b29a83f6f470441ec
Ryan Kelly <94ryan.kelly@gmail.com>
2017-12-15 17:17:52 +0000

    First implementation of FluentAssertions.OneOf
    Only OneOf<> is supported as Type inference does not work on constraint types, ruling out OneOfBase<>
    To use type inference all the types must be contained within the parameters.
    I have written the extension methods .Should() Which returns a OneOfAssertion<>
    Be<TResult> is an extension method which attempts to make sure the value of the OneOf is the given case.
    It returns And<TResult> because I can't generically return an item which has been wrapped in an assertion because extension methods are used to initialise them.
    Plus introduced a test lib without fleshing out the tests yet.

---
+++ FluentAssertions.OneOf/AssertionHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using FluentAssertions.Execution;
+
+namespace FluentAssertions.OneOf
+{
+    internal class AssertionHelpers
+    {
+        internal static TResult CheckItemHelper<TItem, TResult>(TItem element,
+            object[] becauseArgs, string because = "", params Type[] expectedTypes)
+        {
+            var itemType = typeof(TItem);
+            var expectedType = typeof(TResult);
+            var expectedTypePretty = PrettyPrint(expectedType);
+            var expectedTypesString = string.Join(", ", expectedTypes.Select(PrettyPrint));
+            Execute.Assertion
+                .ForCondition(expectedTypes.Contains(expectedType))
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Value should be one of {0} but found {1} instead.",
+                    expectedTypesString, expectedTypePretty);
+            
+            Execute.Assertion
+                .ForCondition(itemType == expectedType)
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Value should be type {0} but found {1} instead.",
+                    expectedTypePretty, PrettyPrint(itemType));
+
+            return (TResult)(object)element;
+        }
+
+        internal static string PrettyPrint(Type type)
+        {
+            var genericArguements = type.GetGenericArguments();
+            if (!genericArguements.Any())
+            {
+                return type.Name;
+            }
+            var genericName = type.Name.Substring(0, type.Name.IndexOf("`", StringComparison.Ordinal));
+            return $"{genericName}<{string.Join(",", genericArguements.Select(PrettyPrint))}>";
+        }
+    }
+}

ac835f9cf8b1bcbb177b8bf4980182218249a51f
Ryan Kelly <94ryan.kelly@gmail.com>
2018-02-15 14:38:11 +0000

    Attempting to create nuget package in correct location

--- build.fsx
+++ build.fsx
@@ -73,14 +73,6 @@ Target.Create "Test" (fun _ ->
         })
 )
 
-let displayFolderContents folder =
-    let command =
-        if isWindows then
-            runCommand "ls"
-        else
-            runCommand "dir"
-    command folder globalTimeout
-
 Target.Create "Package" (fun _ ->
     Directory.ensure buildDir
     let finalVersion = versionToPublish.ToString();
@@ -88,10 +80,9 @@ Target.Create "Package" (fun _ ->
         { p with
             TimeOut = globalTimeout;
             Configuration = "Release";
-            OutputPath = "..\\build";
+            OutputPath = "../build";
             AdditionalArgs = [ "--no-build"; sprintf "/p:VersionPrefix=\"%s\"" finalVersion ; ]//"--include-source" ;  "--include-symbols"  ]
         })
-    displayFolderContents "build"
 )
 
 let publishPackage version =
